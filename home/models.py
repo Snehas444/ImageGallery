12c7cc151a7353b81bb347b78e22d52bdacf0e36
sneha <snehakatiyar15@gmail.com>
2023-09-17 18:31:07 +0530

    iuysjka

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Upload(models.Model):
+    """Model definition for Upload."""
+
+    caption = models.CharField (max_length=100)
+    image = models.ImageField(upload_to="images/")
+
+    class Meta:
+        """Meta definition for Upload."""
+
+        verbose_name =  'Upload'
+        verbose_name_plural =  'Uploads'
+
+    def __str__(self):
+        return self.caption
+
--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+     
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'main.urls'
-
+ 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -69,14 +71,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =str(BASE_DIR / 'static')
+
+MEDIA_URL ='media/'
+MEDIA_ROOT = str(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
